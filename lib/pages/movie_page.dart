2747e40b4372069c504e32e516a86978d9a2fccd
Teagan Clayton <clay0063@algonquinlive.com>
2023-12-06 21:15:38 -0500

    Change list view to Dismissible view

--- lib/pages/movie_page.dart
+++ lib/pages/movie_page.dart
@@ -44,36 +44,72 @@ class _MoviePageState extends State<MoviePage> {
         child: SafeArea(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: _buildMovieList(),
+            child: _movieCards(),
           ),
         ),
       ),
     );
   }
 
-  Widget _buildMovieList() {
+  Widget _movieCards() {
     return ListView.builder(
       itemCount: movieList.length,
       itemBuilder: (context, index) {
-        return Column(
-          children: <Widget>[
-            Image.network(
-              movieList[index]['image']!,
-              height: 200,
-              fit: BoxFit.contain,
-              errorBuilder: (context, error, stackTrace) {
-                //in case of image load error
-                return const Placeholder(
-                  fallbackHeight: 200,
-                  fallbackWidth: 100,
-                );
-              },
+        return Center(
+            child: Dismissible(
+          key: ValueKey<int>(int.parse(movieList[index]['id'] ?? '0')),
+          onDismissed: (direction) {
+            // Handle dismiss event based on direction
+            if (direction == DismissDirection.endToStart) {
+              // Swiped left (reject)
+              // Handle reject action here
+            } else if (direction == DismissDirection.startToEnd) {
+              // Swiped right (approve)
+              // Handle approve action here
+            }
+            setState(() {
+              movieList.removeAt(index);
+            });
+          },
+          background: Container(
+            color: Colors.green,
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(left: 16.0),
+            child: Icon(
+              Icons.check,
+              color: Colors.white,
             ),
-            Text(movieList[index]['name'] ?? ''),
-            Text(movieList[index]['date'] ?? ''),
-            Text(movieList[index]['rating'] ?? ''),
-            Text(movieList[index]['id'] ?? ''),
-          ],
+          ),
+          secondaryBackground: Container(
+            color: Colors.red,
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.only(right: 16.0),
+            child: Icon(
+              Icons.close,
+              color: Colors.white,
+            ),
+          ),
+          child: Column(
+            children: <Widget>[
+              Image.network(
+                movieList[index]['image']!,
+                height: 200,
+                fit: BoxFit.contain,
+                errorBuilder: (context, error, stackTrace) {
+                  // in case of image load error
+                  return const Placeholder(
+                    fallbackHeight: 200,
+                    fallbackWidth: 100,
+                  );
+                },
+              ),
+              Text(movieList[index]['name'] ?? ''),
+              Text(movieList[index]['date'] ?? ''),
+              Text(movieList[index]['rating'] ?? ''),
+              Text(movieList[index]['id'] ?? ''),
+            ],
+          ),
+        ),
         );
       },
     );
