4a341bf073cc4748ff625a57f723d4ea9a8ad136
Teagan Clayton <clay0063@algonquinlive.com>
2023-12-14 21:19:35 -0500

    update android manifest, add message to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -40,3 +40,16 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+
+//Warning about an error:
+
+//Either iOS or Android running from the terminal (flutter run) works fine
+//iOS running from the Flutter Debug works fine too
+//but Android running from Flutter Debug crashes on the "share code" screen
+//Didn't realize this until too late and can't figure out the issue behind it :/
+
+//The crash is a Paused on exception in socket_patch.dart
+//"No address associated with hostname movie-night-api.onrender.com"
+
+//Tried adding some things to the androidManifest to fix it and it cleared a terminal warning
+//But it still crashes when running through flutter debug! sorry!!
